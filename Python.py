1952dd50f6b6665f6b31bf1c0423ee720b9b8764
Shivansh Raj <113129325+Shivanshraj7654@users.noreply.github.com>
2022-12-08 11:00:17 +0530

    Update Python.py

--- Python.py
+++ Python.py
@@ -1,13 +1,13 @@
 import random
 n = random.randrange(1,10)
-guess = int(input("Enter any number: "))
+guess = int(input("Enter any no.: "))
 while n!= guess:
     if guess < n:
         print("Too low")
-        guess = int(input("Enter number again: "))
+        guess = int(input("Enter no. again: "))
     elif guess > n:
         print("Too high!")
-        guess = int(input("Enter number again: "))
+        guess = int(input("Enter no. again: "))
     else:
       break
 print("you guessed it right!!")
